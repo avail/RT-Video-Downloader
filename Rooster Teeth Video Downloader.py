b5571ff1467631697ea9dbdc1d308283d76d6074
expenses <wruglys@gmail.com>
2015-08-22 20:29:02 +1200

    Tidied up the code and updated it to RT's changes to their html

--- Rooster Teeth Video Downloader.py
+++ Rooster Teeth Video Downloader.py
@@ -1,5 +1,6 @@
-def roosterTeethDownloader():
-    # This is the main function. It basically just acts as a menu for other functions
+import subprocess, os, urllib.request as http
+
+def roosterTeethDownloader(): # This is the main function. It basically just acts as a menu for other functions
     menuOptions = {'D' : 'to enter in the url of a video and download it.',\
                    'I' : 'to manually enter in a index.m3u8 playlist and download a video. This is useful for downloading subscriber-only videos.',\
                    'M' : 'to use MkvToolNix to mux downloaded files into a mkv.',\
@@ -11,7 +12,6 @@ def roosterTeethDownloader():
 
         if response == 'D':
             url = addHttp(input('\nEnter the url of the video:\n'))
-            
             indexFile, videoName = getIndexFile(url)
             
             if indexFile == '':
@@ -24,7 +24,6 @@ def roosterTeethDownloader():
                 videoName = input('No video name found. Please enter one manually:\n')
 
             videoName = cleanVideoName(videoName) # Cleans up the video name to make sure it doesn't contain spaces etc.
-            
             getLinks(videoName, indexFile)
     
         elif response == 'I':
@@ -38,20 +37,17 @@ def roosterTeethDownloader():
             getLinks(videoName, url)
             
         elif response == 'M':
-            
             mkvMuxer(chooseFolder())
             
         elif response == 'X':
-            
             looping = False
 
         print('') # Prints out a blank line at the end because it looks better.
 
-def getLinks(videoName, url):
-    # This function takes the name of a video and a url and gets to work fetching links.
+def getLinks(videoName, url): # This function takes the name of a video and a url and gets to work fetching links.
     resolutions, files, createFile = {}, [], ''
 
-    rootUrl = url.replace('index.m3u8', '') # Sets rootUrl to be the root 'folder' where all of the files are.
+    rootUrl = url.strip('index.m3u8') # Sets rootUrl to be the root 'folder' where all of the files are.
     print('\nDownloading index.m3u8 from ' + rootUrl + '...\n')
     index = readUrl(url) # Loads index as the contents of the url.
     print(index)
@@ -81,15 +77,14 @@ def getLinks(videoName, url):
             
         if response == 'D':
             download(rootUrl, files, videoTitle)
+
         elif response == 'T':
             writeLinksFile(rootUrl, files, videoTitle)
+
         elif response == 'X':
             looping = False
 
-def mkvMuxer(folder):
-    # This function takes a folder and merges all the videos in it.
-    import subprocess, os
-
+def mkvMuxer(folder): # This function takes a folder and merges all the videos in it.
     if folder == '':
         return
 
@@ -108,47 +103,43 @@ def mkvMuxer(folder):
     if not videoCreated:
         print('An error occured in the muxing. Please check that the folder contains valid .ts files.')
 
-def dictionarify(aList, remove):
-    # This function takes a list and a string of characters to remove and turns it into a dictionary.
+def dictionarify(aList, remove): # This function takes a list and a string of characters to remove and turns it into a dictionary.
     dictionary = {}
+    
     for item in aList:
-        dictionary[item.replace(remove, '')] = ''
+        dictionary[item.strip(remove)] = ''
+
     return dictionary
 
-def readUrl(url):
-    # This function requests a url (with firefox headers, roosterteeth.com rejects the default python headers) and returns its contents, decoded into utf-8.
-    import urllib.request
-
+def readUrl(url): # This function requests a url (with firefox headers, roosterteeth.com rejects the default python headers) and returns its contents, decoded into utf-8.
     headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:39.0) Gecko/20100101 Firefox/39.0"}
-    urlRequest = urllib.request.Request(url, headers = headers)
-    return urllib.request.urlopen(urlRequest).read().decode('utf-8')
+    urlRequest = http.Request(url, headers = headers)
 
-def cleanVideoName(name):
-    # This function cleans up the video name using a list of banned characters.
+    return http.urlopen(urlRequest).read().decode('utf-8')
+
+def cleanVideoName(name): # This function cleans up the video name using a list of banned characters.
     newName = name
     
     for char in ['\\', '/', '*', '?', '"', '<', '>', '|', '&quot;']:
-        newName = newName.replace(char, '')
+        newName = newName.strip(char)
 
     newName = newName.replace(':', ' -').replace(' ', '_')
 
     if not name == newName:
         name = newName
         print('\nVideo renamed to ' + name + ' for compatability.')
+
     return name
 
-def chooseFolder():
-    # This function allows the user to select a folder in 'downloads'.
-    import os
-
-    checkFolder('downloads')
-    
+def chooseFolder(): # This function allows the user to select a folder in 'downloads'.
+    checkFolder('downloads')   
     folders = os.listdir('downloads')
     validFolders = []
     
     for folderName in folders:
         if validFolder('downloads/' + folderName):
             validFolders = validFolders + [folderName]
+
     folders = validFolders
     
     if folders == []:
@@ -162,8 +153,7 @@ def chooseFolder():
 
     return folders[int(input()) - 1]
 
-def removeComments(playlist):
-    # This function takes a raw playlist and returns it as a list, removing lines that are empty and start with a hash.
+def removeComments(playlist): # This function takes a raw playlist and returns it as a list, removing lines that are empty and start with a hash.
     playlist = playlist.split('\n')
     goodLines = []
     
@@ -173,88 +163,75 @@ def removeComments(playlist):
             
     return goodLines
 
-def writeOptionsFile(folder):
-    # This function takes a folder and writes a mkvmerge options file for it.
-    import os
-    
+def writeOptionsFile(folder): # This function takes a folder and writes a mkvmerge options file for it.
     videos = os.listdir('downloads/' + folder) # I should probably validate this so it checks if each item is a .ts file.
     options = 'downloads/' + folder + '/optionsFile.txt'
     print("\nWriting file '" + options + "'....")
     
     file = open(options, 'w')
-
     file.write('--output\ndownloads/' + folder + '.mkv\n--language\n0\ceng\n--language\n1\cund\n(\n') # Eutgh that syntax :/ .
+
     for video in videos:
         if video.endswith('.ts'):
             file.write('downloads/' + folder + '/' + video + '\n')
-    file.write(')\n--track-order\n0\c0,0\c1')
 
+    file.write(')\n--track-order\n0\c0,0\c1')
     file.close()
 
-def writeLinksFile(rootUrl, files, videoTitle):
-    # This function creates a .txt file in 'links' that contains the links of a video.
+def writeLinksFile(rootUrl, files, videoTitle): # This function creates a .txt file in 'links' that contains the links of a video.
     checkFolder('links')
-        
     fileName = 'links/' + videoTitle + '.txt'
 
     file = open(fileName, 'w')
+
     for singularFile in files:
         file.write(rootUrl + singularFile + '\n')
+
     file.close()
-    
     print("\n'" + fileName + "' created.\n")
 
-def download(rootUrl, files, videoTitle):
-    # This function requests and downloads the .ts files.
-    import urllib.request
-
+def download(rootUrl, files, videoTitle): # This function requests and downloads the .ts files.
     checkFolder('downloads')
-    
     directory = 'downloads/' + videoTitle
-
     checkFolder(directory)
-
     print('')
     
     for file in range(0, len(files)):
         print('Downloading ' + files[file] + ' (' + str(file + 1) + '/' + str(len(files)) + ')...')
-        urllib.request.urlretrieve(rootUrl + files[file], directory + '/' + files[file])
+        http.urlretrieve(rootUrl + files[file], directory + '/' + files[file])
+
     print('\nVideo downloading completed.\n')
 
     downloadOptions = {'M' : 'to mux the files into a mkv with mkvmerge.',\
                        'X' : 'to exit'}
     looping = True
+
     while (looping):
         response = validInput('Pick an option:', downloadOptions)
             
         if response == 'M':
             mkvMuxer(videoTitle)
             print('')
+
         elif response == 'X':
             looping = False
 
     print('') # Again, I print out a blank space so that it looks nicer.
 
-def checkFolder(folder):
-    # This function checks if a folder exists, and if it doesn't, it creates it and tells the user about it.
-    import os
-    
+def checkFolder(folder): # This function checks if a folder exists, and if it doesn't, it creates it and tells the user about it.
     if not os.path.exists(folder):
         os.makedirs(folder)
         print("\nFolder '" + folder + "' created.")
 
-def validFolder(folder):
-    # This function uses try to check if an item is indeed a folder but listing its contents. I could have had it check for extensions, but that wouldn't have work if a file didn't have one.
-    import os
-    
+def validFolder(folder): # This function uses try to check if an item is indeed a folder but listing its contents. I could have had it check for extensions, but that wouldn't have work if a file didn't have one.
     try:
         os.listdir(folder)
         return not os.listdir(folder) == []
+
     except NotADirectoryError:
         return False
 
-def validInput(prompt, options):
-    # This function takes a prompt and a dictionary of options and validates the input.
+def validInput(prompt, options): # This function takes a prompt and a dictionary of options and validates the input.
     goodInput = False
     string = prompt + '\n'
 
@@ -271,23 +248,25 @@ def validInput(prompt, options):
             
     return userInput
 
-def addHttp(url):
-    # This function makes sure that if you copy-paste a url from a browser that doesn't display the 'http://' part it puts it in.
+def addHttp(url): # This function makes sure that if you copy-paste a url from a browser that doesn't display the 'http://' part it puts it in.
     if not (url.startswith('http://') or url.startswith('https://')):
         url = 'http://' + url
+
     return url
 
-def getIndexFile(url):
-    # This function takes the url of a video page and returns the url of the index file and the name of the video.
+def getIndexFile(url): # This function takes the url of a video page and returns the url of the index file and the name of the video.
     videoPage = readUrl(url).split('\n')
     indexFile, videoName = '', ''
     
     for line in videoPage: # Sets indexFile and videoName to items in a jwplayer <script> tag in the html.
-        if line.startswith('                                    manifest: '):
-            indexFile = line.replace('                                    manifest: ', '')[1:-2]
-        elif line.startswith('                                videoTitle: '):
-            videoName = line.replace('                                videoTitle: ', '')[1:-2]
-            
+        line = line.replace(' ', '')
+
+        if line.startswith('manifest:'):
+            indexFile = line.strip('manifest:')[1:-2]
+
+        elif line.startswith('varvideoTitle='):
+            videoName = line.strip('varvideoTitle=')[1:-2]
+       
     return indexFile, videoName
 
 roosterTeethDownloader() # This starts the script.
