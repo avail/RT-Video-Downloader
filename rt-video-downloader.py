ec129ab0f1960e58440d4bbf7d098ce5b2088c81
jun <avail@pomf.se>
2017-01-17 20:04:23 +0100

    Default to 1080p if choice is blank

--- rt-video-downloader.py
+++ rt-video-downloader.py
@@ -8,26 +8,26 @@ def roosterTeethDownloader(): # This is the main function. It basically just act
                    'J' : 'to concatenate downloaded files into a single file.',\
                    'X' : 'to exit.'}
     looping = True
-    
+
     while (looping): # Loops the options until it is told to quit.
         response = validInput('Pick an option:', menuOptions)
 
         if response == 'D':
             url = addHttp(input('\nEnter the url of the video:\n'))
             indexFile, videoName = getIndexFile(url)
-            
+
             if indexFile == '':
                 print('An index.m3u8 file was not found. Please check that you have entered the correct url and that the video isn\'t on youtube instead.\n')
                 return roosterTeethDownloader()
 
             print('\nIndex file found at:\n' + indexFile + '\n')
-            
+
             if videoName == '':
                 videoName = input('No video name found. Please enter one manually:\n')
 
             videoName = cleanVideoName(videoName) # Cleans up the video name to make sure it doesn't contain spaces etc.
             getLinks(videoName, indexFile)
-    
+
         elif response == 'I':
             videoName = cleanVideoName(input('\nEnter the name of the video you want to download:\n'))
             url = addHttp(input('\nEnter the url of the index.m3u8 file:\n'))
@@ -37,10 +37,10 @@ def roosterTeethDownloader(): # This is the main function. It basically just act
                 return roosterTeethDownloader()
 
             getLinks(videoName, url)
-            
+
         elif response == 'J':
             concatenate(chooseFolder())
-            
+
         elif response == 'X':
             looping = False
 
@@ -56,7 +56,7 @@ def getLinks(videoName, url): # This function takes the name of a video and a ur
 
     resolutions = dictionarify(removeComments(index), 'P.m3u8') # Sets resolutions to an empty dictionary with the keys being each resolution (minus the P.m3u8 part).
 
-    chosenRes = validInput('Which resolution would you like to download?\nAvaliable resolutions:', resolutions) # Makes sure the input is only of the featured resolutions.
+    chosenRes = validInput('Which resolution would you like to download?\nAvaliable resolutions:', resolutions, True) # Makes sure the input is only of the featured resolutions.
     videoTitle = videoName + '_-_' + chosenRes
     url = rootUrl + chosenRes + 'P.m3u8'
 
@@ -66,17 +66,17 @@ def getLinks(videoName, url): # This function takes the name of a video and a ur
 
     for file in removeComments(resolutionPlaylist):
         files.append(file)
-        
+
     print(str(len(files)) + ' files found (it begins with ' + files[0] + ').\n')
 
     downloadOptions = {'D' : 'to download the files with python. This may be slower than using a download manager.',\
                        'T' : 'to create a text file of the download links.',\
                        'X' : 'to exit'}
     looping = True
-    
+
     while (looping):
         response = validInput('Pick an option:', downloadOptions)
-            
+
         if response == 'D':
             download(rootUrl, files, videoTitle)
 
@@ -98,7 +98,7 @@ def concatenate(folder): # This function takes a folder and merges all the files
 
 def dictionarify(aList, remove): # This function takes a list and a string of characters to remove and turns it into a dictionary.
     dictionary = {}
-    
+
     for item in aList:
         dictionary[item.strip(remove)] = ''
 
@@ -112,7 +112,7 @@ def readUrl(url): # This function requests a url (with firefox headers, roostert
 
 def cleanVideoName(name): # This function cleans up the video name using a list of banned characters.
     newName = name
-    
+
     for char in ['\\', '/', '*', '?', '"', '<', '>', '|', '&quot;']:
         newName = newName.strip(char)
 
@@ -125,35 +125,35 @@ def cleanVideoName(name): # This function cleans up the video name using a list 
     return name
 
 def chooseFolder(): # This function allows the user to select a folder in 'downloads'.
-    checkFolder('downloads')   
+    checkFolder('downloads')
     folders = os.listdir('downloads')
     validFolders = []
-    
+
     for folderName in folders:
         if validFolder('downloads/' + folderName):
             validFolders = validFolders + [folderName]
 
     folders = validFolders
-    
+
     if folders == []:
         print("There are no valid folders in 'downloads'. Please download a video or add a folder to that location.")
         return ''
 
-    print("\nChoose the id of the folder in 'downloads' that contains the .ts files you want muxed:") 
-        
+    print("\nChoose the id of the folder in 'downloads' that contains the .ts files you want muxed:")
+
     for name in range(0, len(folders)):
         print(str(name + 1) + ': ' + folders[name])
 
     return folders[int(input()) - 1]
 
-def removeComments(playlist): # This function takes a raw playlist and returns it as a list, removing lines that are empty and start with a hash.
+def removeComments(playlist): # This function takesvalidInput a raw playlist and returns it as a list, removing lines that are empty and start with a hash.
     playlist = playlist.split('\n')
     goodLines = []
-    
+
     for line in playlist:
         if not line.startswith('#') and not line == '':
             goodLines.append(line)
-            
+
     return goodLines
 
 def writeLinksFile(rootUrl, files, videoTitle): # This function creates a .txt file in 'links' that contains the links of a video.
@@ -169,7 +169,7 @@ def writeLinksFile(rootUrl, files, videoTitle): # This function creates a .txt f
     print("\n'" + fileName + "' created.\n")
 
 
-def isInteger(i): # Checks if "i" is an integer. 
+def isInteger(i): # Checks if "i" is an integer.
 
     try:
         int(i)
@@ -192,13 +192,13 @@ def download(rootUrl, files, videoTitle): # This function requests and downloads
     numThreads = 'NaN'
     while (not isInteger(numThreads)):
         numThreads = input('Give me a number of threads to use:')
-    
+
     numThreads = int(numThreads)
     filesPerThread = len(files) // numThreads # Integer division only.
     remainderFiles = len(files) %  numThreads # Give these to last thread.
     threadArray = []
 
-    start = time.time()    
+    start = time.time()
     for i in range(0, numThreads):
         a = i * filesPerThread
         b = a + filesPerThread
@@ -212,7 +212,7 @@ def download(rootUrl, files, videoTitle): # This function requests and downloads
 
     for i in range(0, numThreads):
         threadArray[i].join()
-    
+
     print('\nVideo downloading completed.')
     end = time.time()
     print('Time elapsed: ', end - start, 'sec\n')
@@ -223,7 +223,7 @@ def download(rootUrl, files, videoTitle): # This function requests and downloads
 
     while (looping):
         response = validInput('Pick an option:', downloadOptions)
-            
+
         if response == 'J':
             concatenate(videoTitle)
             print('')
@@ -246,22 +246,25 @@ def validFolder(folder): # This function uses try to check if an item is indeed 
     except NotADirectoryError:
         return False
 
-def validInput(prompt, options): # This function takes a prompt and a dictionary of options and validates the input.
+def validInput(prompt, options, default = False): # This function takes a prompt and a dictionary of options and validates the input.
     goodInput = False
     string = prompt + '\n'
 
     for option in sorted(options):
         string = string + option + ' ' + options[option] + '\n'
-        
+
     while not goodInput:
         userInput = input(string)
+
+        if default == True and userInput == "": # if input was blank, default to 1080p
+            return "NewHLS-1080"
 
         for option in options:
             if userInput.lower() == option.lower():
                 userInput = option
                 goodInput = True
                 break
-            
+
     return userInput
 
 def addHttp(url): # This function makes sure that if you copy-paste a url from a browser that doesn't display the 'http://' part it puts it in.
